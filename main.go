a019bc431fe75a27d38893d5f167f9a345220b42
Milad Doorbash <milad.doorbash@gmail.com>
2019-09-15 08:54:05 +0430

    add support for multiple clients

--- main.go
+++ main.go
@@ -16,48 +16,65 @@ var upgrader = websocket.Upgrader{
 	CheckOrigin:     func(r *http.Request) bool { return true },
 }
 
-var logchan chan []byte = make(chan []byte)
+var clientsChan chan *websocket.Conn = make(chan *websocket.Conn)
+var logsChan chan []byte = make(chan []byte)
 
-func runWs() {
+func main() {
+	go func() {
+		// cmd := exec.Command("ping", "-t", "google.com")
+		cmd := exec.Command("pm2", "logs")
+		cmdReader, err := cmd.StdoutPipe()
+		if err != nil {
+			log.Fatal(err)
+		}
+		scanner := bufio.NewScanner(cmdReader)
+		if err := cmd.Start(); err != nil {
+			log.Fatal(err)
+		}
+		for scanner.Scan() {
+			data := scanner.Bytes()
+			// fmt.Println(string(data))
+			logsChan <- data
+		}
+	}()
+
+	go func() {
+		var clients map[*websocket.Conn]bool = make(map[*websocket.Conn]bool)
+		var clientsRemoveList []*websocket.Conn = make([]*websocket.Conn, 0)
+		for {
+			select {
+			case client := <-clientsChan:
+				clients[client] = true
+			case data := <-logsChan:
+				for client := range clients {
+					if err := client.WriteMessage(websocket.TextMessage, data); err != nil {
+						client.Close()
+						clientsRemoveList = append(clientsRemoveList, client)
+						continue
+					}
+				}
+				for _, clientToRemove := range clientsRemoveList {
+					delete(clients, clientToRemove)
+				}
+			}
+
+		}
+	}()
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		http.ServeFile(w, r, "./index.html")
 	})
 
-	http.HandleFunc("/pm2logs", func(w http.ResponseWriter, r *http.Request) {
-		var conn, _ = upgrader.Upgrade(w, r, nil)
-		go func(conn *websocket.Conn) {
-			for {
-				conn.WriteMessage(websocket.TextMessage, <-logchan)
-			}
-		}(conn)
+	http.HandleFunc("/logs", func(w http.ResponseWriter, r *http.Request) {
+		var conn, err = upgrader.Upgrade(w, r, nil)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		clientsChan <- conn
 	})
-	err := http.ListenAndServe(":3030", nil)
-	if err != nil {
+
+	if err := http.ListenAndServe(":3030", nil); err != nil {
 		fmt.Println(err)
 	}
-}
-
-func runPm2Log() {
-	cmd := exec.Command("pm2", "logs", "--json")
-	cmdReader, err := cmd.StdoutPipe()
-	if err != nil {
-		log.Fatal(err)
-	}
-	scanner := bufio.NewScanner(cmdReader)
-	go func() {
-		for scanner.Scan() {
-			logchan <- scanner.Bytes()
-		}
-	}()
-	if err := cmd.Start(); err != nil {
-		log.Fatal(err)
-	}
-	// if err := cmd.Wait(); err != nil {
-	// 	log.Fatal(err)
-	// }
-}
-
-func main() {
-	go runPm2Log()
-	runWs()
 }
